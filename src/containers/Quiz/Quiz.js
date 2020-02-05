b5de28a025c839d239afea16ba5be288f72f9379
Volodymyr H <frontend.ukrinsoft@invitedesk.com>
2020-02-05 07:52:39 +0200

    Init

---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import classes from './Quiz.module.css'
+class Quiz extends Component {
+    render () {
+        return (
+            <div className={classes.Quiz}>
+                <h1>Quiz</h1>
+            </div>
+        )
+    }
+}
+
+export default Quiz
